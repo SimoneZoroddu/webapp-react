c4b1cc68f49274072dfb6b5828f9799bfc180883
SimoneZoroddu <lenviur@gmail.com>
2026-04-21 16:39:24 +0200

    adding Axios call for Homepage with all the films and single film in the page film

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,8 +1,45 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+import { useParams } from "react-router-dom"
+
+export default function Film() {
 
 
-export default function Film(){
+    const { filmId } = useParams()
+
+    console.log(filmId);
+
+    const [film, setFilm] = useState([])
+
+    useEffect(() => {
+
+        const api_url = import.meta.env.VITE_API_ADDRESS + '/movies'
+
+        axios.get(`${api_url}/${filmId}`)
+            .then(datas => {
+                //console.log(datas.data)
+                setFilm([datas.data])
+            }
+            )
+    }, [])
 
     return (
-        <h1>Film singolo</h1>
+        <div className="container">
+            <div className="row">
+                {
+                    film.map((film, index) => (
+                        <div key={index} className="col">
+                            <div className="card" style={{ width: '18rem' }}>
+                                <img src={`http://localhost:3010/${film.title}.jpg`} className="card-img-top" alt={film.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{film.title}</h5>
+                                    <p className="card-text">{film.director}{film.genre}{film.release_year}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,38 @@
-
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
 export default function HomePage() {
 
+    const [films, setFilms] = useState([])
+
+    useEffect(() => {
+        const api_url = import.meta.env.VITE_API_ADDRESS + '/movies'
+
+        axios.get(api_url)
+            .then(datas => {
+                console.log(datas.data)
+                setFilms(datas.data)
+            }
+            )
+    }, [])
+
     return (
-        <h1>Homepage</h1>
+        <div className="container">
+            <div className="row">
+                {
+                    films.map((film, index) => (
+                        <div key={index} className="col">
+                            <div className="card" style={{ width: '18rem' }}>
+                                <img src={`http://localhost:3010/${film.title}.jpg`} className="card-img-top" alt={film.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{film.title}</h5>
+                                    <p className="card-text">{film.director}{film.genre}{film.release_year}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
